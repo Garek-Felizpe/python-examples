2a9b91d1143629d9f161c2663e57a679e3126c49
Felipe <philldso@gmail.com>
2025-11-04 18:29:41 -0300

    adding new scripts of my learning process

---
+++ app/loop.py
@@ -0,0 +1,10 @@
+pkm_inicial = ["Pikachu", "Squirtle", "Bubassauro", "Charmander"]
+
+for pokemon in pkm_inicial:
+    print(pokemon)
+
+contador = 0
+
+while contador < 5:
+    print(contador)
+    contador += 0.5
--- app/main.py
+++ app/main.py
@@ -1,46 +1,60 @@
 class HelloWorld:
     def __init__(self):
         pass
-    
+
     def hello(self):
         print("Hello world! This is my firts Python Project!")
 
+
 class Operator:
-    def __init__(self):
-        pass
+    a: int
+    b: int
+
+    def __init__(self, a: int, b: int):
+        self.a = a
+        self.b = b
 
     def callOperator(self):
-        a = 10
-        b = 3
-        equal = a == b   # False
-        print("Este resultado deve ser igual a",equal)
+        equal = self.a == self.b  # False
+        print("Este resultado deve ser igual a", equal)
         # print(f"Este resultado deve ser igual a:{equal}")
-        different = a != b   # True
-        print("Este resultado deve ser igual a",different)
-        bigger_than = a > b   # True
-        print("Este resultado deve ser igual a",bigger_than)
-        smaller_than = a < b   # False
-        print("Este resultado deve ser igual a",smaller_than)
-        bigger_or_equal = a >= b   # True
-        print("Este resultado deve ser igual a",bigger_or_equal)
-        smaller_or_equal = a <= b   # False
-        print("Este resultado deve ser igual a",smaller_or_equal)
-        result_and = (a > 5) and (b < 5)   # True
-        print("Este resultado deve ser igual a",result_and)
-        result_or = (a > 15) or (b < 5)   # True
-        print("Este resultado deve ser igual a",result_or)
-        result_not = not (a > 5)   # False
-        print("Este resultado deve ser igual a",result_not)
+        different = self.a != self.b  # True
+        print("Este resultado deve ser igual a", different)
+        bigger_than = self.a > self.b  # True
+        print("Este resultado deve ser igual a", bigger_than)
+        smaller_than = self.a < self.b  # False
+        print("Este resultado deve ser igual a", smaller_than)
+        bigger_or_equal = self.a >= self.b  # True
+        print("Este resultado deve ser igual a", bigger_or_equal)
+        smaller_or_equal = self.a <= self.b  # False
+        print("Este resultado deve ser igual a", smaller_or_equal)
+        result_and = (self.a > 5) and (self.b < 5)  # True
+        print("Este resultado deve ser igual a", result_and)
+        result_or = (self.a > 15) or (self.b < 5)  # True
+        print("Este resultado deve ser igual a", result_or)
+        result_not = not (self.a > 5)  # False
+        print("Este resultado deve ser igual a", result_not)
 
+
+class Loop:
+    pkm_inicial = ["Pikachu", "Squirtle", "Bubassauro", "Charmander"]
+
+    def pokemon(self):
+
+        for pokemon in self.pkm_inicial:
+            print(pokemon)
 
 
 def main():
     print("Script Started")
-    my_object=HelloWorld()
-    my_object.hello()
-    my_object=Operator()
-    my_object.callOperator()
+
+    hello_world = HelloWorld()
+    hello_world.hello()
+    operator = Operator()
+    operator.callOperator()
+    loop = Loop()
     print("Script Finished")
+
 
 if __name__ == "__main__":
     main()
