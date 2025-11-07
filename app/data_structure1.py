83ba1fe12c530e7949768b9bb27a0f62a162cd02
Felipe S. Oliveira <philldso@gmail.com>
2025-11-06 22:31:53 -0300

    Some notes about .slice

--- app/data_structure1.py
+++ app/data_structure1.py
@@ -2,7 +2,7 @@ zelda_characters = ["Link", "Zelda", "Ganon", "Sheik", "Midna", "Darunia", "Navi
 zelda_characters.append("Kafei")
 zelda_characters.insert(13, "Anju")
 zelda_characters.remove("Tingle")
-zelda_characters_triforce = zelda_characters.pop(4)
+zelda_characters_triforce = zelda_characters.pop(4) # Removes "Midna", but if you want to remove more dynamically, you can use index -1 to remove the last item, or search for the index of a specific item.
 print(zelda_characters)
 print("Removed character:", zelda_characters_triforce)
 zelda_characters.sort()
