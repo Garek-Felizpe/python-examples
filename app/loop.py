156895507c47c33bd34bc0fc8c67743e672a2c51
Felipe S. Oliveira <philldso@gmail.com>
2025-11-06 22:06:47 -0300

    More of my learning process

--- app/loop.py
+++ app/loop.py
@@ -4,7 +4,17 @@ for pokemon in pkm_inicial:
     print(pokemon)
 
 contador = 0
-
-while contador < 5:
+while True:
     print(contador)
-    contador += 0.5
+    contador += 1
+    if contador == 5:
+        break
+
+for i in range(10):
+
+    if i%2==0:
+        continue
+    print(i)
+
+for i in range(5):
+    pass
